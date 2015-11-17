da259c5ee79157dd4ba82efb73c33d880516a7ea
Nicklas Laine Overgaard <nicklas@skarp.dk>
2015-11-17 16:08:45 +0100

    The error handler is now a configurable provider
    The error handler factory / http interceptor is now a provider so that
    the inner error and success functions are configurable from the app.
    This means that the error handler is no longer depending on rootScope as
    the actual error handling is now delegated to the app using the error
    handler.
    
    Updated app.js to configure the error handler with the previously
    implemented inner functions.

--- src/NgValidationErrors/js/app.js
+++ src/NgValidationErrors/js/app.js
@@ -9,17 +9,39 @@
         "helloApp.controllers",
         "navigation.controllers",
         "users.controllers",
-        "errorHandling",
+        "requestErrorHandler",
         "ssValidationDecorator"
     ]);
 
-    module.config(["$routeProvider", "$locationProvider", function ($routeProvider, $locationProvider) {
+    module.config(["$routeProvider", "$locationProvider", "requestErrorHandlerProvider", function ($routeProvider, $locationProvider, requestErrorHandlerProvider) {
         $routeProvider.when("/", { templateUrl: "/partials/signup/signup.html", controller: "signUpCtrl" });
         $routeProvider.when("/users", { templateUrl: "/partials/users/list.html", controller: "userListCtrl" });
         $routeProvider.when("/404", { templateUrl: "/partials/404.html" });
         $routeProvider.otherwise({ redirectTo: "/404" });
 
         $locationProvider.html5Mode(true);
+
+        requestErrorHandlerProvider.setResponseFn(function (response, $injector) {
+            var rootScope = $injector.get("$rootScope");
+            rootScope.validationErrors = null;
+        });
+
+        requestErrorHandlerProvider.setResponseErrorFn(function(rejection, $injector) {
+            var toastr = $injector.get("toastr");
+            var $rootScope = $injector.get("$rootScope");
+            if (rejection.status === 500) {
+                var msg = "Something terrible went wrong there.";
+                if (rejection.data.ResponseStatus) {
+                    msg = rejection.data.ResponseStatus.Message;
+                }
+                toastr.error(msg, "Server is haywire");
+            } else if (rejection.status === 400) { // user provided garbage!
+                $rootScope.validationErrors = rejection.data.ResponseStatus.Errors;
+
+            } else {
+                toastr.error("Seems like you killed the server - we are working on it", "Error");
+            }
+        });
     }]);
 
 })();
--- src/NgValidationErrors/js/errorHandling/requestErrorHandler.js
+++ src/NgValidationErrors/js/errorHandling/requestErrorHandler.js
@@ -1,67 +1,84 @@
 ﻿/* global angular */
 // code taken and adapted from article: http://www.codelord.net/2014/06/25/generic-error-handling-in-angularjs/
+// largest adaption is making the error and response success functions configurable on the app.
 (function() {
-    var HEADER_NAME = "MyApp-Handle-Errors-Generically";
+    var HEADER_NAME = "ng-generic-error-handler-module";
     var specificallyHandleInProgress = false;
 
-    var app = angular.module("errorHandling", []);
-    app.factory("RequestsErrorHandler", requestErrorHandlerFactory);
+    var app = angular.module("requestErrorHandler", []);
+    app.provider("requestErrorHandler", function requestErrorHandlerProvider() {
 
-    requestErrorHandlerFactory.$inject = ["$q", "$log", "$injector", "$rootScope"];
-    function requestErrorHandlerFactory ($q, $log, $injector, $rootScope) {
-        return {
-            // --- The user's API for claiming responsiblity for requests ---
-            specificallyHandled: function(specificallyHandledBlock) {
-                specificallyHandleInProgress = true;
-                try {
-                    return specificallyHandledBlock();
-                } finally {
-                    specificallyHandleInProgress = false;
-                }
-            },
+        // default response error function
+        var responseErrorFn = function (rejection, $injector) {
+            var log = $injector.get("$log");
+            log.error("In default request error handler, configure me!", rejection);
+        }
 
-            response: function(response) {
-                var shouldHandle = (response && response.config && response.config.headers
-                    && response.config.headers[HEADER_NAME]);
-                
-                if (!shouldHandle) return response;
+        // default response function
+        var responseFn = function(response, $injector) {
+            return;
+        }
+        /**
+         * Set the error handling function.
+         * Please note that two variables are injected - the "rejection" promise and the angular $injector so you can resolve other services while error handling
+         * @param {} fn The function to use for error handling
+         * @returns {} 
+         */
+        this.setResponseErrorFn = function(fn) {
+            responseErrorFn = fn;
+        }
 
-                $rootScope.validationErrors = null;
-                return response;
-            },
+        /**
+         * Set the success full response handling function. I.e to clear state after previous errors
+         * @param {} fn The function to use for response handling
+         * @returns {} 
+         */
+        this.setResponseFn = function(fn) {
+            responseFn = fn;
+        }
 
-            // --- Response interceptor for handling errors generically ---
-            responseError: function(rejection) {
-                var shouldHandle = (rejection && rejection.config && rejection.config.headers
-                   && rejection.config.headers[HEADER_NAME]);
-
-                if (shouldHandle) {
-                    $log.error(rejection);
-                    var toastr = $injector.get("toastr");
-
-                    if (rejection.status === 500) {
-                        var msg = "Something terrible went wrong there.";
-                        if (rejection.data.ResponseStatus) {
-                            msg = rejection.data.ResponseStatus.Message;
+        this.$get = ["$q", "$injector",
+            function ($q, $injector) {
+                return {
+                    // --- The user's API for claiming responsiblity for requests ---
+                    specificallyHandled: function(specificallyHandledBlock) {
+                        specificallyHandleInProgress = true;
+                        try {
+                            return specificallyHandledBlock();
+                        } finally {
+                            specificallyHandleInProgress = false;
                         }
-                        toastr.error(msg, "Server is haywire");
-                    } else if (rejection.status === 400) { // user provided garbage!
-                        $rootScope.validationErrors = rejection.data.ResponseStatus.Errors;
+                    },
 
-                    } else {
-                        toastr.error("Seems like you killed the server - we are working on it", "Error");
+                    // --- Response interceptor for handling sucessfull requests generically ---
+                    response: function(response) {
+                        var shouldHandle = (response && response.config && response.config.headers
+                            && response.config.headers[HEADER_NAME]);
+
+                        if (shouldHandle) {
+                            responseFn(response, $injector);
+                        }
+                        return response;
+                    },
+
+                    // --- Response interceptor for handling errors generically ---
+                    responseError: function(rejection) {
+                        var shouldHandle = (rejection && rejection.config && rejection.config.headers
+                            && rejection.config.headers[HEADER_NAME]);
+
+                        if (shouldHandle) {
+                            responseErrorFn(rejection, $injector);
+                        }
+                        return $q.reject(rejection);
                     }
-                }
-                return $q.reject(rejection);
+                };
             }
-        };
-    };
-    
-    
+        ];
+    });
 
     app.config([
         "$provide", "$httpProvider", function($provide, $httpProvider) {
-            $httpProvider.interceptors.push("RequestsErrorHandler");
+            $httpProvider.interceptors.push("requestErrorHandler");
 
             // --- Decorate $http to add a special header by default ---
 
